a7135c958ec0b8230d7b3c01fea6ce7fec770a4d
Lucas Galfasó <lgalfaso@gmail.com>
2014-04-08 11:44:24 -0300

    automate cutting a release

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,13 @@
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-npm');
 
     grunt.initConfig({
+      jshint: {
+        all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
+      },
       karma: {
         unit: { configFile: 'karma.conf.js' },
         autotest: {
@@ -22,8 +27,17 @@
           browsers: ['PhantomJS']
         }
       },
-      jshint: {
-        all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
+      bump: {
+        files: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json'],
+        tagName: '%VERSION%',
+        pushTo: 'origin'
+      },
+      'npm-publish': {
+         options: {
+           requires: ['jshint', 'karma:unit', 'bump'],
+           abortIfDirty: true
+         }
       }
     });
   };
