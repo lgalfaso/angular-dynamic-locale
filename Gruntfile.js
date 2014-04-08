1bc381377a274f425751b21546a46036c1ed8d29
Lucas Galfasó <lgalfaso@gmail.com>
2014-04-08 11:57:06 -0300

    Fix grunt-bump configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,10 +28,12 @@
         }
       },
       bump: {
-        files: ['package.json', 'bower.json'],
-        commitFiles: ['package.json', 'bower.json'],
-        tagName: '%VERSION%',
-        pushTo: 'origin'
+        options: {
+          files: ['package.json', 'bower.json'],
+          commitFiles: ['package.json', 'bower.json'],
+          tagName: '%VERSION%',
+          pushTo: 'origin'
+        }
       },
       'npm-publish': {
          options: {
