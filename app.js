4e82cd3b0a701fe5f72a97f19dd8e44300adc366
Lucas Galfaso <lgalfaso@gmail.com>
2014-07-16 18:04:30 +0200

    feat(gh-pages): Initial page demo

---
+++ app.js
@@ -0,0 +1,20 @@
+angular.module('DynamicLocaleExample', ['tmh.dynamicLocale'])
+  .config(function(tmhDynamicLocaleProvider) {
+    tmhDynamicLocaleProvider.localeLocationPattern('https://code.angularjs.org/1.2.20/i18n/angular-locale_{{locale}}.js');
+  })
+  .controller('ExampleCtrl', function($rootScope, tmhDynamicLocale, $locale) {
+    $rootScope.availableLocales = {
+      'en': 'English',
+      'de': 'German',
+      'fr': 'French',
+      'ar': 'Arabic',
+      'ja': 'Japanese',
+      'ko': 'Korean',
+      'zh': 'Chinese'};
+    $rootScope.model = {selectedLocale: 'en'};
+    $rootScope.$locale = $locale;
+    $rootScope.$watch('model.selectedLocale', function(newValue) {
+      tmhDynamicLocale.set(newValue); 
+    });
+  });
+
