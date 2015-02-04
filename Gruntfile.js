a288ab90925c123557cf6060c81903b3850a33c7
Lucas Galfaso <lgalfaso@gmail.com>
2015-02-05 00:05:19 +0100

    chore(version): do not push the tag to origin on bump
    Do not push the tag on bump as it has the wrong banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,8 +71,7 @@
         options: {
           files: ['package.json', 'bower.json'],
           commitFiles: ['package.json', 'bower.json'],
-          tagName: '%VERSION%',
-          pushTo: 'origin'
+          tagName: '%VERSION%'
         }
       },
       'npm-publish': {
